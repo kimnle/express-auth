f67e523a6700eff3dae50975ab2203e3c9e9f99d
Kim <72332310+kimnle@users.noreply.github.com>
2024-11-05 20:59:56 +1100

    feat: user auth-gated routes

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { User } = require("./models/UserModel");
+const { generateJWT, validateUserAuth } = require("./functions/jwtFunctions");
 
 const app = express();
 
@@ -11,6 +13,39 @@ app.use(express.json());
 app.get("/", (request, response) => {
     response.json({
         message: "Hello, world!!"
+    });
+});
+
+app.post("/signup", async(request, response) => {
+    // check that a username and password are provided in request.body
+    let username = request.body.username;
+    let password = request.body.password;
+
+    if (!username || !password) {
+        response.status(400).json({
+            message: "Incorrect or missing sign-up creditials provided."
+        });
+    }
+
+    // make a user in the DB using the username and password
+    let newUser = await User.create({username: username, password: password});
+
+    // make a JSW based on the username and userID
+    let newJwt = generateJWT(newUser.id, newUser.username);
+
+    // return the JWT
+    response.json({
+        jwt: newJwt,
+        user: {
+            id: newUser.id,
+            username: newUser.username
+        }
+    });
+});
+
+app.get("/protectedRoute", validateUserAuth, (request, response) => {
+    response.json({
+        message: "You can see protected content because you're signed in!!"
     });
 });
 
